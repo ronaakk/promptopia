842b4e2e3892f3f626197af674612e440f6a2f6b
Ronak Patel <ronaakk6@gmail.com>
2024-11-21 15:15:31 -0500

    working on next auth still

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,7 @@ import { get } from "http"
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { connectToDB } from "../../../../utils/database"
+import User from "../../../../models/User"
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -12,20 +13,30 @@ export const authOptions = {
     }),
   ],
 
+  // updating the user session
   async session({ session }) {
-
+    const sessionUser = await User.findOne({email: session.user.email})
+    session.user.id = sessionUser._id.toString()
+    return session
   },
+
+  // handling sign in
   async signIn({ profile }) {
     try {
       await connectToDB();
 
       // check if a user already exists
-
-
-      // if not, create a new user, and save to db
-
+      const user = await User.findOne({email: profile.email})
       
-
+      if (!user) {
+        // if not, create a new user, and save to db
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture
+        })
+      }
+      
       return true
     } catch (error) {
       console.log('Something went wrong when signing in, please try again. ', error)
@@ -34,5 +45,5 @@ export const authOptions = {
   }
 }
 
-export default NextAuth(authOptions)
+export { handler as GET, handler as POST }
 // the [...nextauth] will capture all routes related to next auth
--- models/User.js
+++ models/User.js
@@ -1,18 +1,23 @@
-import { Schema, model, models } from "mongoose";
-import { type } from "os";
+import mongoose, { Schema, model, models } from "mongoose";
+
 
 const UserSchema = new Schema({
     email: {
         type: String,
         // if it already exists, the second argument will be displayed
-        unique: [true, 'Email already exists'],
+        unique: [true, 'Email already exists.'],
         required: [true, 'Please provide your email!']
     },
     username: {
-        unique: [true, 'Username already exists'],
-        required: [true, 'Username is required to proceed!']
+        unique: [true, 'Username already exists.'],
+        required: [true, 'Username is required to proceed!'],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
+    },
+    image: {
+        type: String,
     }
 })
 
-const userModel = model('User', UserSchema)
-export default userModel
+// check whether the User model already exists, if not create one
+const User = models.User || mongoose.model('User', UserSchema)
+export default User
