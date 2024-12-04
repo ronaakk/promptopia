19addeb9551c7f5355aef0fb42ff14f191d5a790
Ronak Patel <ronaakk6@gmail.com>
2024-12-03 21:12:42 -0500

    working on only allowing one tag per prompt

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { useState } from 'react'
+import { ReactElement, ReactEventHandler, ReactNode, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 import Form from '@/components/Form'
-import 
+
 
 export default function CreatePrompt() {
     const [submitting, isSubmitting] = useState(false) 
@@ -14,15 +14,27 @@ export default function CreatePrompt() {
     })
     
     // create the prompt and save it to db
-    const createPrompt = async (e): Promise<void> => {
+    const createPrompt = async (e: React.FormEvent): Promise<void> => {
         e.preventDefault()
         isSubmitting(true)
 
         try {
-            // try and save the new prompt to the model
+            // try and save the new prompt to the model using api
+            const response = await fetch('/api/prompt/new', {
+                method: 'POST',
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    tag: post.tag.split('')
+                })
+            })
             
-        } catch (error: Error) {
-            console.log('Failed to post/edit prompt: ', error.message)
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.log('Failed to post/edit prompt: ', error.message)
+            } else {
+                console.log('Unknown error: ', error)
+            }
+
         }
     }
 
