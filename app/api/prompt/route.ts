e924cdfb2e958f8f74eecc8f989cd5b27fa1dbe4
Ronak Patel <ronaakk6@gmail.com>
2024-12-12 14:51:10 -0500

    working on homepage feed and searching ability

---
+++ app/api/prompt/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest } from "next/server";
+import Prompt from "@/models/Prompt";
+import { connectToDB } from "@/utils/database";
+
+// Get all posts
+export async function GET(req: NextRequest) {
+    try {  
+        await connectToDB();
+
+        // retrieve all posts from database, while populating the creator field with info about the user
+        const posts = await Prompt.find({}).populate("creator")
+
+        return Response.json(posts, { status: 200 })
+    } catch (error) {
+        console.log('Error retrieving all posts: ', error)
+        return Response.json({ error: "Error retrieving all posts." }, { status: 500 })
+    }
+}
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,54 @@
+"use client"
+import { useEffect, useState } from "react"
 
-function Feed() {
+
+async function Feed() {
+  // use a state variable to store all posts
+  const [allPosts, setAllPosts] = useState([])
+
+  // search states for search bar
+  const [searchText, setSearchText] = useState('')
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchedResults, setSearchedResults] = useState([])
+
+  // get all posts using api
+  const fetchAllPosts = async () => {
+    const response = await fetch('/api/prompt')
+    const data = await response.json()
+    setAllPosts(data)
+  }
+
+  // we want to get all posts on intial render of homepage
+  useEffect(() => {
+    fetchAllPosts()
+  }, [])
+
+  // debounce method (reduce api calls and improving performance in search results)
+  // will wait for user to stop typing
+  const handleSearchChange = (e) => {
+
+    // reset the current search timeout
+    setSearchTimeout(0)
+
+
+  }
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input 
+          type="text" 
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          className="search_input peer"
+
+        />
+      </form>
+
+
+    
+    </section>
   )
 }
 
